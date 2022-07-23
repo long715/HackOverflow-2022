9096e520edc46ee04de12d5f7d9bd3b02a3ce3c8
Lee-Violet <leevioletong@gmail.com>
2022-07-23 16:23:48 +1200

    implemented media

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import Character from "./resources/images/download.jpg";
+import Character from "./resources/images/owl.gif";
 import React, {useState} from 'react';
 
 function App() {
   const [onPage, setOnPage] = useState(false);
 
   return (
-    <div className="bg-linen h-screen w-screen">
+    <div className={`bg-[url('/public/img/static-bg.png')] h-screen w-screen`}>
       <div className ="flex justify-center items-center">
         <button className={`bg-white ${onPage? "block":"hidden"}`}>Map</button>
-        <div onClick={()=> setOnPage(!onPage)}>
+        <div className="w-32 h-32" onClick={()=> setOnPage(!onPage)}>
           <img src={Character} alt="this is an image" className="cursor-pointer"/>
         </div>
       </div>
