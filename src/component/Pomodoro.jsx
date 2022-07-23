08b3550579ba9b52632acebeae4a357899d2e29e
mattwai <mwai409@aucklanduni.ac.nz>
2022-07-23 22:46:08 +1200

    play/pause button

--- src/component/Pomodoro.jsx
+++ src/component/Pomodoro.jsx
@@ -5,43 +5,29 @@ function Pomodoro() {
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(10);
   const [displayMessage, setDisplayMessage] = useState(false);
-  const [count, setCount] = useState(1);
-  const interval = useRef(null);
-
-  const startTimer = () => {
-    if (interval.current == null) {
-      interval.current = setInterval(() => {
-        setSeconds((seconds) => Math.max(seconds - 1, 0));
-      }, 1000);
-    }
-  };
-
-  const stopTimer = () => {
-    if (interval.current != null) {
-      clearInterval(interval.current);
-      interval.current = null;
-    }
-  };
+  const [play, pause] = useState(false);
 
   useEffect(() => {
-    // let interval = setInterval(() => {
-    //   clearInterval(interval);
-    //   if (seconds === 0) {
-    //     if (minutes !== 0) {
-    //       setSeconds(59);
-    //       setMinutes(minutes - 1);
-    //     } else {
-    //       let minutes = displayMessage ? 0 : 0;
-    //       let seconds = 5;
-    //       setSeconds(seconds);
-    //       setMinutes(minutes);
-    //       setDisplayMessage(!displayMessage);
-    //     }
-    //   } else {
-    //     setSeconds(seconds - 1);
-    //   }
-    // }, 1000);
-  }, [seconds, minutes]);
+    if (play) {
+      let interval = setInterval(() => {
+        clearInterval(interval);
+        if (seconds === 0) {
+          if (minutes !== 0) {
+            setSeconds(59);
+            setMinutes(minutes - 1);
+          } else {
+            let minutes = displayMessage ? 0 : 0;
+            let seconds = 5;
+            setSeconds(seconds);
+            setMinutes(minutes);
+            setDisplayMessage(!displayMessage);
+          }
+        } else {
+          setSeconds(seconds - 1);
+        }
+      }, 1000);
+    }
+  }, [seconds, play]);
   const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
   const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
 
@@ -60,11 +46,8 @@ function Pomodoro() {
         </div>
       </div>
       <div className="playPauseButtons">
-        <button className="start-button" onClick={startTimer}>
-          start
-        </button>
-        <button className="stop-button" onClick={stopTimer}>
-          stop
+        <button className="start-button" onClick={() => pause(!play)}>
+          Play | Pause
         </button>
       </div>
     </div>
