8ef91db990098f04a9541412c590c685692328a0
Ruth Gao <zgao258@aucklanduni.ac.nz>
2022-07-23 16:20:36 +1200

    Revert "CancelChecklist"
    This reverts commit 05e4ee1236ccb4a0e8b76dd55900005c78d62fb1.

---
+++ src/component/CheckList.jsx
@@ -0,0 +1,42 @@
+//code adapted from https://codesandbox.io/s/todo-list-hooks-ebfgw?file=/src/App.js
+
+import React, { useState } from 'react';
+import data from "./CheckList/Data.json";
+import Header from "./CheckList/Header.jsx";
+import ToDoList from "./CheckList/ToDoList";
+import ToDoForm from './CheckList/ToDoForm';
+
+function CheckList() {
+  
+  const [ toDoList, setToDoList ] = useState(data);
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
+
+  const addTask = (userInput ) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
+
+  return (
+    <div className="CheckList">
+      <Header />
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+      <ToDoForm addTask={addTask}/>
+    </div>
+  );
+}
+
+export default CheckList;
