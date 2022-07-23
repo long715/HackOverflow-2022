258e9e673c1f237c0d74b7fe48d54061c8bd3059
Lee-Violet <leevioletong@gmail.com>
2022-07-23 16:30:46 +1200

    final owl navigatio

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,11 @@ function App() {
   return (
     <div className={`bg-[url('/public/img/static-bg.png')] h-screen w-screen`}>
       <div className ="flex justify-center items-center">
-        <button className={`bg-white ${onPage? "block":"hidden"}`}>Map</button>
+        <button className={`bg-white ${onPage? "block":"hidden"} w-24 h-24 bg-[#CCEBE7] border-[#511414] border-2`}>-1 Chapter</button>
         <div className="w-32 h-32" onClick={()=> setOnPage(!onPage)}>
           <img src={Character} alt="this is an image" className="cursor-pointer"/>
         </div>
+        <button className={`bg-white ${onPage? "block":"hidden"} w-24 h-24 bg-[#CCEBE7] border-[#511414] border-2`}>+1 Chapter</button>
       </div>
     </div>
   );
