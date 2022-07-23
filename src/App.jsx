c2104fb80100a18cdaf49a01774013df7abcded4
Lee-Violet <leevioletong@gmail.com>
2022-07-24 10:32:26 +1200

    notebook and pdf view

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Dropzone from "./component/Dropzone"
 import React, {useState} from 'react';
 import ReadingStatus from "./component/ReadingStatus";
+import Notebook from "./component/Notebook";
 
 function App() {
 
   return (
     <div className={`bg-[url('/public/img/static-bg.png')] h-screen w-screen`}>
       <ReadingStatus/>
-      <Dropzone/>
+      <Notebook/>
+      
 
     </div>
   );
