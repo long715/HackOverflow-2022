870676abae9cf9d17b8c91cd50a875dc46ef94ae
Ruth Gao <zgao258@aucklanduni.ac.nz>
2022-07-23 16:04:27 +1200

    Checklist'

---
+++ src/component/CheckList/ToDoList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ToDo from './ToDo';
+
+//the container that holds all of todos
+const ToDoList = ({toDoList, handleToggle, handleFilter}) => {
+    return (
+        <div>
+            {toDoList.map(todo => {
+                return (
+                    <ToDo todo={todo} handleToggle={handleToggle} handleFilter={handleFilter}/>
+                )
+            })}
+            <button style={{margin: '20px'}} onClick={handleFilter}>Clear Completed</button>
+        </div>
+    );
+};
+
+export default ToDoList;
